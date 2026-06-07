59d006ebe016f7032de8f34b7e0154234077f7ed
Mtaijim shaikh <mhdtaijim07@gmail.com>
2026-06-07 13:40:34 +0530

    chore: initialize spring boot project

---
+++ AuthNext/AuthNext/src/main/java/com/example/AuthNext/AuthNextApplication.java
@@ -0,0 +1,13 @@
+package com.example.AuthNext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthNextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthNextApplication.class, args);
+	}
+
+}
