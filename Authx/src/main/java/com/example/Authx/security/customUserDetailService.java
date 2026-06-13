9da8c981d23f8fca872be49c9d8502e59c58ef2a
Mtaijim shaikh <mhdtaijim07@gmail.com>
2026-06-13 22:47:24 +0530

    feat(auth): setup register authentication with Spring Security

---
+++ Authx/src/main/java/com/example/Authx/security/customUserDetailService.java
@@ -0,0 +1,23 @@
+package com.example.Authx.security;
+
+
+import com.example.Authx.exceptions.ResourceNotFoundException;
+import com.example.Authx.repositories.userRepository;
+
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NonNull;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class customUserDetailService implements UserDetailsService {
+    private final  userRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(@NonNull String username) throws UsernameNotFoundException {
+    return  userRepository.findByEmail(username).orElseThrow(()-> new ResourceNotFoundException("Invalid Email"));
+    }
+}
--- Authx/src/main/java/com/example/Authx/services/Impl/AuthServiceImpl.java
+++ Authx/src/main/java/com/example/Authx/services/Impl/AuthServiceImpl.java
@@ -5,16 +5,19 @@ import com.example.Authx.services.AuthService;
 import com.example.Authx.services.UserService;
 
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class AuthServiceImpl implements AuthService {
     private final UserService userService;
+    private  final PasswordEncoder passwordEncoder;
     @Override
     public UserDto registerUser(UserDto userDto) {
 //        verify email etc logic ....
-       UserDto userDto1 = userService.createUser(userDto);
-        return  userDto1;
+        userDto.setPassword(passwordEncoder.encode(userDto.getPassword()));
+         return userService.createUser(userDto);
+
     }
 }
