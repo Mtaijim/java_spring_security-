268b17a82ed26e8df4edf52f9b47c90112856de8
Mtaijim shaikh <mhdtaijim07@gmail.com>
2026-06-17 03:45:48 +0530

    feat: implement JWT authentication service

--- Authx/src/main/java/com/example/Authx/config/securityConfig.java
+++ Authx/src/main/java/com/example/Authx/config/securityConfig.java
@@ -1,39 +1,64 @@
 package com.example.Authx.config;
 
 
+
+import com.example.Authx.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import java.util.Map;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import tools.jackson.databind.ObjectMapper;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class securityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
   @Bean
 public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     return http
-            .csrf(csrf -> csrf.disable())
+            .csrf(AbstractHttpConfigurer::disable)
+            .cors(Customizer.withDefaults())
+            .sessionManagement(s->s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
                     .requestMatchers("/api/v1/auth/register", "/api/v1/auth/login")
                     .permitAll()
                     .anyRequest()
                     .authenticated()
             )
-            .httpBasic(Customizer.withDefaults())
-            .build();
+         .exceptionHandling(ex -> ex.authenticationEntryPoint((request, response, e) -> {
+                    e.printStackTrace();
+                    response.setStatus(401);
+                    response.setContentType("application/json");
+                    String message = e.getMessage();
+
+        Map<String, String> errorMap = Map.of(
+                "Message", message,
+                "Status", String.valueOf(401),
+                "statusCode", Integer.toString(401)
+        );
+        var objectMapper = new ObjectMapper();
+response.getWriter().write(objectMapper.writeValueAsString(errorMap));
+    }
+                )).addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
 }
 
 }
